107425daeb27ba5f30d35f8048b31265d092e785
anthonydubeau <anthonydubeau@outlook.com>
2025-10-02 15:03:16 -0400

    Fini

--- labo2.py
+++ labo2.py
@@ -2,12 +2,11 @@ def salutations(nom):
     return "Bonjour "+ nom
 
 print (salutations("antho"))
-<<<<<<< HEAD
+
 
 
 def age(annee):
     return "Votre âge est " + str(2025-annee)+ " ans."
 
 print(age(2003))
-=======
->>>>>>> 03f06fd0be4fae490f0522acef68026991c31044
+
